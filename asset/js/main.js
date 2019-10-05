c9ebc214128b004b981ce7bb8d1fba701edae8d5
emanmoq <42995029+emanmoq@users.noreply.github.com>
2019-10-05 11:18:02 -0700

    hh

--- asset/js/main.js
+++ asset/js/main.js
@@ -86,6 +86,51 @@ $(document).ready(function () {
   }
 
     
+  $("#lightSlider").lightSlider({
+    item: 1,
+    autoWidth: false,
+    slideMove: 1, // slidemove will be 1 if loop is true
+    slideMargin: 10,
+
+    addClass: '',
+    mode: "slide",
+    useCSS: true,
+    cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+    easing: 'linear', //'for jquery animation',////
+
+    speed: 400, //ms'
+    auto: false,
+    loop: false,
+    slideEndAnimation: true,
+    pause: 2000,
+
+    keyPress: false,
+    controls: true,
+    prevHtml: '<i class="lni-chevron-left"></i>',
+    nextHtml: '<i class="lni-chevron-right"></i>',
+
+    rtl:false,
+    adaptiveHeight:false,
+
+    vertical:false,
+    verticalHeight:100,
+    vThumbWidth:200,
+
+    thumbItem:6,
+    gallery: true,
+    galleryMargin: 5,
+    currentPagerPosition: 'middle',
+
+    enableTouch:true,
+    enableDrag:true,
+    freeMove:true,
+    swipeThreshold: 40,
+
+    responsive : [],
+
+
+});
+
+});
 
                 
-});
